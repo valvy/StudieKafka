a0c24d12cb6aca9b6e33630f1f51a996887a25b3
Heiko van der Heijden <h.vanderheijden@eaglescience.nl>
2020-04-14 19:06:55 +0200

    some tidying up

--- chapter2/src/main/scala/nl/hvanderheijden/chapter2/Main.scala
+++ chapter2/src/main/scala/nl/hvanderheijden/chapter2/Main.scala
@@ -4,32 +4,53 @@ import java.util.logging.Logger
 import nl.hvanderheijden.chapter2.core.{Producer, Reader}
 import nl.hvanderheijden.chapter2.producers.{Enricher, Validator, Writer}
 
+import scala.Option
+
 object Main {
 
   val logger: Logger = Logger.getLogger(Main.getClass.getName)
 
+
+
   def main(args: Array[String]): Unit = {
     this.logger.info("Starting monedero")
 
-    // TODO: make sure its wrapped in a maybe.
-    val servers: String = "localhost:29092"
-    val groupId: String = "foo"
-    val inputTopic: String = "input-topic"
-    val validTopic: String = "valid-messages"
-    val invalidTopic: String = "invalid-messages"
-
-    def getProducer(arg: String): Producer = {
-      arg.toLowerCase match {
-        case "enricher" => new Enricher(servers, validTopic, invalidTopic)
-        case "validator" => new Validator(servers, validTopic, invalidTopic)
-        case "writer" => new Writer(servers, validTopic)
+    def getFromItem(element: Int): Option[String] = {
+      if(args.length > element) {
+          Some(args(element))
+      } else {
+        None
       }
     }
 
-    val reader = new Reader(servers, groupId, inputTopic)
-    val validator = getProducer("validator")
-    reader.run(validator)
+    // TODO: make sure its wrapped in a maybe.
+    val servers: String = getFromItem(0).getOrElse( "localhost:29092")
 
+    val groupId: String = getFromItem(1).getOrElse("foo")
+    val inputTopic: String = getFromItem(2).getOrElse("input-topic")
+    val validTopic: String = getFromItem(3).getOrElse("valid-messages")
+    val invalidTopic: String = getFromItem(4).getOrElse("invalid-messages")
+
+    def getProducer(arg: String): Option[Producer] = {
+      arg.toLowerCase match {
+        case "enricher" => Some(new Enricher(servers, validTopic, invalidTopic))
+        case "validator" => Some(new Validator(servers, validTopic, invalidTopic))
+        case "writer" => Some(new Writer(servers, validTopic))
+        case _ => None
+      }
+    }
+
+    val producer = getFromItem(5).getOrElse("writer")
+
+    val reader = new Reader(servers, groupId, inputTopic)
+    getProducer(producer) match {
+      case Some(x) => {
+        logger.info(s"using producer ${ producer}")
+        reader.run(x)
+
+      }
+      case _ => logger.info("could not recognise the producer.")
+    }
   }
 
 
